5354fc84e7e06959826aaad5b9ac2d0131e6c732
Cong Cuong PHAM <ccuong.ph@gmail.com>
2017-07-10 03:11:56 +0200

    19. Get URL Links & Get Absolute URL: - Add name for URL link in posts/urls.py. - Add get_absolute_url function in models.py, import reverse module. - get_absolute_url function return url address with reverse method. - Add namespace for posts app in trydjango19/urls.py. Body in detail

--- posts/models.py
+++ posts/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.urlresolvers import reverse
 
 # Create your models here.
 class Post(models.Model):
@@ -9,3 +10,7 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse("detail", kwargs={"id": self.id}) # return reversed url corresponding with self.id
+        # return "posts/%s/" %(self.id)
--- posts/urls.py
+++ posts/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+
 from .views import (
     post_list,
     post_create,
@@ -9,7 +10,7 @@ from .views import (
 urlpatterns = [
     url(r'^$', post_list),
     url(r'^create/$', post_create),
-    url(r'^detail/(?P<id>\d+)/$', post_detail),
+    url(r'^(?P<id>\d+)/$', post_detail, name="posts.detail"), # add name for link
     url(r'^update/$', post_update),
     url(r'^delete/$', post_delete),
 ]

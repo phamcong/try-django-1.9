d6401665bd0913dea78ecafe13237e9f046580b7
Cong Cuong PHAM <ccuong.ph@gmail.com>
2017-07-10 02:04:46 +0200

    initiate project

---
+++ posts/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import url
+from .views import (
+    post_list,
+    post_create,
+    post_detail,
+    post_update,
+    post_delete,
+)
+urlpatterns = [
+    url(r'^$', post_list),
+    url(r'^create/$', post_create),
+    url(r'^detail/(?P<id>\d+)/$', post_detail),
+    url(r'^update/$', post_update),
+    url(r'^delete/$', post_delete),
+]
